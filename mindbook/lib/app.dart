85d3b9c3d5966229b892fd5758bebf9f0ba13d9c
denisa-git <28634418+denisa-git@users.noreply.github.com>
2020-04-11 20:30:45 +0100

    project: create basic app folder structure

---
+++ mindbook/lib/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+class MindbookApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mindbook',
+      home: HomePage(),
+      );
+  }
+}

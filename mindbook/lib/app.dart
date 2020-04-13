93c91be00a51013368db4ce748885cea7634df3b
denisa-git <28634418+denisa-git@users.noreply.github.com>
2020-04-13 15:21:12 +0100

    home: BottomAppBar testing

--- mindbook/lib/app.dart
+++ mindbook/lib/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'home.dart';
+import 'package:mindbook/home.dart';
 
 class MindbookApp extends StatelessWidget {
   @override
--- mindbook/lib/home.dart
+++ mindbook/lib/home.dart
@@ -4,9 +4,28 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Mindbook'),
-      ),
+      bottomNavigationBar: BottomAppBar(
+          color: Colors.white,
+          child: new Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.menu),
+                onPressed: null,
+              ),
+              IconButton(
+                icon: Icon(Icons.search),
+                onPressed: null,
+              )
+            ],
+          )),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton.extended(
+          elevation: 4.0,
+          icon: const Icon(Icons.add),
+          label: const Text('Add an event'),
+          onPressed: null),
     );
   }
 }
